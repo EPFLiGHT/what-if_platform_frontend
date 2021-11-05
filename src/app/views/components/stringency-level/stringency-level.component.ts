4c2f505a2244b2a1e8a45bc1315ff58cd3173299
Francesco <posa.francesco98@gmail.com>
2021-11-05 17:48:34 +0100

    clear stringency icon added

--- src/app/views/components/stringency-level/stringency-level.component.ts
+++ src/app/views/components/stringency-level/stringency-level.component.ts
@@ -1,17 +1,25 @@
+import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stringency-level',
   templateUrl: './stringency-level.component.html',
-  styleUrls: ['./stringency-level.component.scss']
+  styleUrls: ['./stringency-level.component.scss'],
 })
 export class StringencyLevelComponent implements OnInit {
-  @Input("stringency") stringency;
-  @Input("maxLevel") maxLevel;
+  @Input('stringency') stringency;
+  @Input('maxLevel') maxLevel;
 
-  constructor() { }
+  ctrl: FormControl;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.ctrl = new FormControl(null, Validators.required);
+    this.ctrl.setValue(this.stringency);
   }
 
+  toKebabCase(str) {
+    return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+  }
 }
